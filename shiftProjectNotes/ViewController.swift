20ff6feea5a49be811cdeae66a710aadf40c0767
sema_alimpiev <jonerty04@gmail.com>
2022-12-26 18:46:36 +0700

    Initial Commit

---
+++ shiftProjectNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shiftProjectNotes
+//
+//  Created by Семён Алимпиев on 26.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
